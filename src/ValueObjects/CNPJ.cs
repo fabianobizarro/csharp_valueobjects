c440a13a23666901c0b23cf5338c868843a709ca
Fabiano Bizarro <fabianoasbizarro@gmail.com>
2020-04-10 14:44:58 -0300

    initial commit

---
+++ src/ValueObjects/CNPJ.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ValueObjects
+{
+    public sealed class CNPJ
+    {
+        public string Numero { get; }
+
+        public CNPJ(string numero)
+        {
+            if (string.IsNullOrWhiteSpace(numero))
+                throw new ArgumentException("message", nameof(numero));
+
+            if (!Valido(numero))
+                throw new Exception("excecao");
+
+            Numero = numero;
+        }
+
+        private static bool Valido(string cnpj)
+        {
+            if (string.IsNullOrWhiteSpace(cnpj))
+                return false;
+
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+
+            if (cnpj.Length != 14)
+            {
+                return false;
+            }
+
+            var tempCnpj = cnpj.Substring(0, 12);
+            var soma = 0;
+
+            for (int i = 0; i < 12; i++)
+            {
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            }
+
+            var resto = (soma % 11);
+
+            resto = resto < 2
+                ? 0
+                : 11 - resto;
+
+            var digito = resto.ToString();
+            tempCnpj += digito;
+            soma = 0;
+
+            for (int i = 0; i < 13; i++)
+            {
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            }
+
+            resto = (soma % 11);
+
+            resto = resto < 2
+                ? 0
+                : 11 - resto;
+
+            digito += resto.ToString();
+
+            return cnpj.EndsWith(digito);
+        }
+
+        public override string ToString() => ToString(true);
+
+        public string ToString(bool removerMascara = false)
+        {
+            return !removerMascara
+                ? Numero
+                : Numero.Replace(".", string.Empty)
+                        .Replace("/", string.Empty)
+                        .Replace("-", string.Empty);
+        }
+
+        public static bool TryParse(string valor, out CNPJ cnpj)
+        {
+            try
+            {
+                cnpj = new CNPJ(valor);
+                return true;
+            }
+            catch (Exception)
+            {
+                cnpj = null;
+                return false;
+            }
+        }
+
+        public bool Equals(CNPJ other)
+        {
+            return other is null
+                ? false
+                : Numero == other.Numero;
+        }
+
+        public static implicit operator CNPJ(string value) => new CNPJ(value);
+    }
+}
---
+++ src/ValueObjects/UF.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace ValueObjects
+{
+    public sealed class UF
+    {
+        private static readonly Dictionary<string, string> _dict = new Dictionary<string, string>
+        {
+            { "AC", "Acre" },
+            { "AL", "Alagoas" },
+            { "AP", "Amapá" },
+            { "AM", "Amazonas" },
+            { "BA", "Bahia" },
+            { "CE", "Ceará" },
+            { "DF", "Distrito Federal" },
+            { "ES", "Espírito Santo" },
+            { "GO", "Goiás" },
+            { "MA", "Maranhão" },
+            { "MT", "Mato Grosso" },
+            { "MS", "Mato Grosso do Sul" },
+            { "MG", "Minas Gerais" },
+            { "PA", "Pará" },
+            { "PB", "Paraíba" },
+            { "PR", "Paraná" },
+            { "PE", "Pernambuco" },
+            { "PI", "Piauí" },
+            { "RJ", "Rio de Janeiro" },
+            { "RN", "Rio Grande do Norte" },
+            { "RS", "Rio Grande do Sul" },
+            { "RO", "Rondônia" },
+            { "RR", "Roraima" },
+            { "SC", "Santa Catarina" },
+            { "SP", "São Paulo" },
+            { "SE", "Sergipe" },
+            { "TO" ,"Tocantins" }
+        };
+
+        public string Sigla { get; }
+
+        public string Nome { get; }
+
+        public UF(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                throw new ArgumentException("message", nameof(valor)); // TODO: MEnsagemde erro 
+            }
+
+            var nomeNormalizado = Normalizado(valor);
+
+            if (_dict.Keys.Contains(nomeNormalizado))
+            {
+                Sigla = nomeNormalizado;
+                Nome = _dict[nomeNormalizado];
+            }
+            else if (_dict.Values.Any(v => Normalizado(v) == nomeNormalizado))
+            {
+                var kv = _dict.FirstOrDefault(p => Normalizado(p.Value) == nomeNormalizado);
+                Sigla = kv.Key;
+                Nome = kv.Value;
+            }
+            else
+            {
+                throw new Exception("sdfsdfsdf"); // TODO: MEnsagemde erro 
+            }
+        }
+
+        private static string RemoverAcentos(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                return valor;
+            }
+
+            var normalized = valor.Normalize(NormalizationForm.FormD);
+            var sb = new StringBuilder();
+
+            foreach (var c in normalized)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString()
+                .Normalize(NormalizationForm.FormC);
+        }
+
+        private static string Normalizado(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                return valor;
+            }
+
+            return RemoverAcentos(valor).ToUpper();
+        }
+
+        public string ToString(bool sigla) => sigla ? Sigla : Nome;
+
+        public override string ToString() => ToString(true);
+
+        public static bool TryParse(string valor, out UF uf)
+        {
+            try
+            {
+                uf = new UF(valor);
+                return true;
+            }
+            catch
+            {
+                uf = null;
+                return false;
+            }
+        }
+    }
+}
